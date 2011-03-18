1c9f937d669d5d92e6d4e9c2c912314119ed9911
Robbie Wolfe <rob.33x@gmail.com>
2011-03-18 17:18:13 -0400

    Implemented the Logger class 	modified:   EOBC/src/Common/logger.cpp 	modified:   EOBC/src/Common/logger.h

--- EOBC/src/Common/logger.cpp
+++ EOBC/src/Common/logger.cpp
@@ -1,7 +1,39 @@
-
-
 #include "logger.h"
+#include <QDate>
+#include <QTime>
+#include <QFile>
+#include <QTextStream>
 
-Logger::Logger()
+const QString Logger::OUTPUT_FILE = "logfile.log";
+const char* Logger::errorString = "!!!! ERROR MESSAGE called";
+const char* Logger::debugString = "~~~~ DEBUG MESSAGE called";
+const char* Logger::infoString = "@@@@ INFO MESSAGE called";
+
+Logger::Logger(char* callingFunction, char *errorMessage, LogType type)
 {
+    QFile file(OUTPUT_FILE);
+    if (file.open(QIODevice::Append))
+    {
+        QTextStream stream(&file);
+
+        stream << "\n---------------------------------------------------------------\n";
+        stream << "On" << QDate::currentDate().toString("ddd MMMM d yy") << ": " << QTime::currentTime().toString("h:m:ss ap");
+
+        switch (type)
+        {
+        case Logger::DEBUG:
+            stream << debugString;
+            break;
+        case Logger::ERROR:
+            stream << errorString;
+            break;
+        case Logger::INFO:
+            stream << infoString;
+            break;
+        }
+        stream << "from function: " << callingFunction << "\n";
+        stream << "Message:\n";
+        stream << errorMessage;
+        stream << "\n---------------------------------------------------------------\n";
+    }
 }
--- EOBC/src/Common/logger.h
+++ EOBC/src/Common/logger.h
@@ -1,12 +1,34 @@
-
-
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <QString>
+#include <QTextStream>
+
+/**
+ * Logger class used for debugging and error checking
+ * Easy to use, when created it will output to a log file.
+ * It has different message priorities.
+ *
+ * @todo what to do about traceability
+ */
 class Logger
 {
 public:
-    Logger();
+    enum LogType {
+        ERROR,
+        DEBUG,
+        INFO
+    };
+
+    Logger(char* callingFunction, char* errorMessage, LogType type);
+
+    static const QString OUTPUT_FILE;
+    static const char* errorString;
+    static const char* debugString;
+    static const char* infoString;
+
+private:
+
 };
 
 #endif // LOGGER_H

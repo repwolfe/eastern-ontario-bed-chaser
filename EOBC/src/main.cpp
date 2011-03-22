d01315d4c779c48e07a0715807d5e49bed4e7c6d
Robbie Wolfe <rob.33x@gmail.com>
2011-03-22 19:15:13 -0400

    Removing my temporary testing 	modified:   EOBC/src/main.cpp

--- EOBC/src/main.cpp
+++ EOBC/src/main.cpp
@@ -1,7 +1,6 @@
 #include <QtGui/QApplication>
 #include "DisplayMap/displaymapcontrol.h"
 #include "LogOn/logoncontrol.h"
-#include "Model/area.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,34 +9,5 @@ int main(int argc, char *argv[])
     mapControl.run();
     LogOnControl logControl;
     logControl.run();
-
-    /***************** Testing Model ****************/
- // TEMPORARY: REMOVING IT SOON -Robbie
-    QMap<ID,Area*> areas;
-    for (int i = 0; i < 5; ++i)
-    {
-	areas.insert(i, new Area(i));
-    }
-
-    Area* area4 = areas[4];
-
-    for (int i = 500; i < 510; ++i)
-    {
-	Facility* fac = new Facility(i, "Rawr", 1, 2, QPoint(0,0));
-	area4->addFacility(fac);
-    }
-
-    // Add Patient John to Area 4, Facility 508
-    Facility* fac = area4->getFacility(508);
-    fac->addPatientToBed(new Patient("2341245", "John", "Doe", EOBC::AC), EOBC::CCC);
-
-    // Move Patient John to Area 4, Facility 2
-    // Remove Patient John
-
-    foreach(Area* area, areas)
-    {
-	delete area;
-    }
-
     return a.exec();
 }
